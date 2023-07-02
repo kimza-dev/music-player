07f797dc8f4ff7285d97f1067f09c743b0fac598
mosaajii <mosaajiihub@gmail.com>
2023-07-02 13:05:25 +0300

    api/models

--- music_backend/api/models.py
+++ music_backend/api/models.py
@@ -17,6 +17,6 @@ def generate_unique_code():
 class Room(models.Model):
     code = models.CharField(max_length=10, default="", unique=True)
     host = models.CharField(max_length=50, unique=True)
-    guest_can_pause = models.CharField(null=False, default=False)
+    guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
